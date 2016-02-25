c5f7b364dade2b216460a6ad45321fe7ad79a6d7
Anton Petrov <petrov.anton@live.com>
2016-02-25 14:24:55 +0300

    All ok.

---
+++ src/edu/petrov/gojavaonline/module01/GoJavaOnlineFirstProgram.java
@@ -0,0 +1,14 @@
+package edu.petrov.gojavaonline.module01;
+
+/**
+ * GoJavaOnline - Module01
+ *
+ * @author a.petrov
+ * @version 1.0
+ */
+public class GoJavaOnlineFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
