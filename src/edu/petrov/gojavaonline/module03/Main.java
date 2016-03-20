324a3886c668459b0166bf8dfba2715e1bc0aceb
Anton Petrov <petrov.anton@live.com>
2016-03-20 16:28:36 +0300

    Module07

--- src/edu/petrov/gojavaonline/module03/Main.java
+++ src/edu/petrov/gojavaonline/module03/Main.java
@@ -1,36 +1,10 @@
 package edu.petrov.gojavaonline.module03;
 
-import edu.petrov.gojavaonline.module03.filesystem.Directory;
-import edu.petrov.gojavaonline.module03.filesystem.FileSystem;
-import edu.petrov.gojavaonline.module03.filesystem.ImageFile;
-import edu.petrov.gojavaonline.module03.filesystem.TextFile;
-
 /**
  * TODO: write simple command line
  * Created by anton on 06/03/16.
  */
 public class Main {
     public static void main(String[] args) {
-        Directory base = FileSystem.createDirectory("C:/petrov", "base");
-        base.createFile("file1");
-        base.createFile("file2");
-        base.createFile("file2");
-        base.createFile(new TextFile("mytext.txt"));
-
-        ImageFile imgFile = new ImageFile("image1.jpg");
-        imgFile.setImage(
-                "  /\\_/\\\n" +
-                        "=( °w° )=\n" +
-                        "  )   (  //\n" +
-                        " (__ __)//"
-        );
-        imgFile.drawImage();
-
-        base.createFile(imgFile);
-        base.createDirectory("dir1").createDirectory("dir2").createFile("file3");
-
-        base.print(true);
-
-        base.print();
     }
 }
