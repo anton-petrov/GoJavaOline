63a2dfd38354e09fd99976e40d186ed7cc2c7f58
Anton Petrov <petrov.anton@live.com>
2016-04-21 21:01:04 +0300

    Practice #1

---
+++ src/edu/petrov/gojavaonline/codegym/Main.java
@@ -0,0 +1,134 @@
+package edu.petrov.gojavaonline.codegym;
+
+class JoinCharacters {
+    public int join(char[] input) {
+        int output = 0;
+        for (int i = input.length - 1, r = 1; i >= 0; i--, r *= 10) {
+            output += Character.getNumericValue(input[i]) * r;
+        }
+        return output;
+    }
+}
+
+class SumDigits {
+    public int sum(int number) {
+        int sum = 0;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return Math.abs(sum);
+    }
+}
+
+class FirstOddNumber {
+    public int find(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 != 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+class MatrixSnakeTraversal {
+    public int[] print(int[][] input) {
+
+        int length = 0;
+        for (int i = 0; i < input.length; i++) {
+            length += input[i].length;
+        }
+        int result[] = new int[length];
+        int p = 1;
+        int i = 0;
+        int k = 0;
+        for (int j = 0; j < input[0].length; j++) {
+            while (i >= 0 && i < input.length) {
+                result[k++] = input[i][j];
+                i += p;
+            }
+            p = -p;
+            i += p;
+        }
+        return result;
+    }
+}
+
+class MatrixTraversal {
+    public int[] print(int[][] input) {
+        if (input == null || input.length == 0 || input[0].length == 0) return new int[0];
+        int n = input.length;
+        int m = input[0].length;
+        int[] output = new int[n * m];
+        if ( m == 1) {
+            output[0] = input[0][0];
+            return output;
+        }
+        int f = n-1;
+        int w = m-1;
+
+        int c = 0;
+        int i = 0;
+        int j = 0;
+        int p = 0;
+
+        while (c != n*m-1)
+        {
+            while (j < w)
+            {
+                output[c++] = input[i][j];
+                j++;
+            }
+            while (i < f)
+            {
+                output[c++] = input[i][j];
+                i++;
+            }
+            while (j > p)
+            {
+                output[c++] = input[i][j];
+                j--;
+            }
+            f--;
+            w--;
+            p++;
+            while (i > p)
+            {
+                output[c++] = input[i][j];
+                i--;
+            }
+        }
+        if(c == n*m-1)
+        {
+            output[c++] = input[i][j];
+        }
+        for (int e : output) {
+            System.out.print(e + " ");
+        }
+        return output;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(new JoinCharacters().join(new char[]{'1', '2', '3'}));
+        System.out.println(new MatrixTraversal().print(
+                new int[][]
+                        {
+                                {1, 2},
+                                {3, 4}
+                        }
+
+        ));
+//        System.out.println(new FirstOddNumber().find(new int[]{1, 1, 2, 2, 3}));
+//        System.out.println(new MatrixSnakeTraversal().print(new int[][]
+//                {
+//                        {1, 1, 2},
+//                        {3, 3, 4},
+//                        {5, 5, 6}
+//                }));
+//        System.out.println(new SumDigits().sum(123));
+    }
+}
